2a155af7ea18de2d58326e9a6051366266aac9f2
Yamuna63 <yamuna@s.amity.edu.com>
2025-07-07 20:51:21 +0530

    dream part added

--- int.c
+++ int.c
@@ -5,3 +5,5 @@ int main()
 	pritnf(" MY name is yamuna")
 printf(" my faher name is rudresh and mother is poornima and brother is prashanth")
 printf("currently am percuing B.tech CSE in amity university banglore")
+printf("i want to become a software engineer and wnat high package salary")
+
