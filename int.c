9ea0abd0cc76c3d09b0ee8cd27f24c53255b04ae
Yamuna63 <yamuna@s.amity.edu.com>
2025-07-14 12:00:02 +0530

    added {}

--- int.c
+++ int.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 int main()
+{	
 	printf(" introduction")
-
 	pritnf(" MY name is yamuna")
-printf(" my faher name is rudresh and mother is poornima and brother is prashanth")
-printf("currently am percuing B.tech CSE in amity university banglore")
-printf("i want to become a software engineer and wnat high package salary")
-
+        printf(" my faher name is rudresh and mother is poornima and brother is prashanth")
+        printf("currently am percuing B.tech CSE in amity university banglore")
+        printf("i want to become a software engineer and wnat high package salary")
+   return o;
+}
