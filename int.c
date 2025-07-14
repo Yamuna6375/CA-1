143e4d1068feded58e457aabdec02f00f36e569f
Yamuna63 <yamuna@s.amity.edu.com>
2025-07-14 11:30:40 +0530

    introduction edited in test1 branch

--- int.c
+++ int.c
@@ -7,3 +7,6 @@ printf(" my faher name is rudresh and mother is poornima and brother is prashant
 printf("currently am percuing B.tech CSE in amity university banglore")
 printf("i want to become a software engineer and wnat high package salary")
 
+now testing code in test1
+printf(" i am manya")
+
